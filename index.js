0c5a8a70f6400b9c518b6f6981ca0127fad80a15
Elliott Brooks <elliott.brooks@xtxmarkets.com>
2023-11-24 12:49:15 +0000

    new pages

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/supporters', (req, res) => {
+  res.render('supporters');
+});
+
+app.get('/get-involved', (req, res) => {
+  res.render('get-involved');
+});
+
 app.listen(process.env.PORT, () => {
   console.log('listening on ' + process.env.PORT);
 });
